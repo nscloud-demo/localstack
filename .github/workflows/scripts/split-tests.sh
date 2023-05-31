2ecb9adc665007d4c9bb725d8be5154af30150f7
Niklas Gehlen <niklas@namespacelabs.com>
2023-05-31 23:11:28 +0200

    Increase parallelism of e2e tests (#4)
    * Run tests with max parallelism.
    
    * remove circleci workflow
    
    * build multi-platform image in single step.
    
    * Debug tag
    
    * fix image tag
    
    * skip artifact download
    
    * Use smaller runners.

--- .github/workflows/scripts/split-tests.sh
+++ .github/workflows/scripts/split-tests.sh
@@ -1,34 +1,18 @@
 #!/bin/sh
 set -e
 
-GROUP_COUNT=$1
-
 shopt -s globstar
-
-COUNTER=0
-for f in ${@:2}
-do
-if [[ $COUNTER -ge $GROUP_COUNT ]]; then
-    COUNTER=0
-fi
-
-echo "$f" >> "split_group_$COUNTER"
-
-COUNTER=$((COUNTER+1))
-done
 
 echo -n "test_paths=[" >> $GITHUB_OUTPUT
 
 COUNTER=0
-for f in split_group_*
+for f in $@
 do
 if [[ $COUNTER -ge 1 ]]; then
     echo -n ", " >> $GITHUB_OUTPUT
 fi
 
-GROUP=`cat "$f" | tr '\n' ' '`
-echo -n "'$GROUP'" >> $GITHUB_OUTPUT
-echo "test group $COUNTER is $GROUP"
+echo -n "'$f'" >> $GITHUB_OUTPUT
 
 COUNTER=$((COUNTER+1))
 done
